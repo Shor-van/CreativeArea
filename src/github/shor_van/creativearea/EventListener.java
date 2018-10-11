aea6f6a6c423f088c8fb91e3a26ec78bcaa43062
Shor-van <shorxvan01@gmail.com>
2018-10-11 20:28:54 +0200

    First commit

---
+++ src/github/shor_van/creativearea/EventListener.java
@@ -0,0 +1,14 @@
+package github.shor_van.creativearea;
+
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class EventListener implements Listener
+{
+	private final JavaPlugin plugin;
+	
+	public EventListener(JavaPlugin plugin)
+	{
+		this.plugin = plugin;
+	}
+}
