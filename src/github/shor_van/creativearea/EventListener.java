8f42bfa226859ee9561f372170b9a383d448de76
Shor-van <shorxvan01@gmail.com>
2018-11-03 08:40:04 +0100

    Updated to use craftbukkit 1.13.2 reference

--- src/github/shor_van/creativearea/EventListener.java
+++ src/github/shor_van/creativearea/EventListener.java
@@ -1,6 +1,10 @@
 package github.shor_van.creativearea;
 
+import java.util.List;
+
+import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**Listens for game events to trigger*/
@@ -16,5 +20,18 @@ public class EventListener implements Listener
             this.plugin = plugin;
         else
             throw new IllegalArgumentException("This class should only be created/passed the creative area plugin's main class");
+    }
+    
+    /**Triggered when a player dies
+     * @param event the PlayerDeathEvent that was triggered*/
+    @SuppressWarnings("unchecked")
+    public void onPlayerDeath(PlayerDeathEvent event)
+    {
+        Player player = event.getEntity();
+        List<String> playersInCreative = (List<String>) plugin.getConfig().getList("players-in-creative");
+        if (playersInCreative.contains(player.getName()) == true)
+        {
+            
+        }
     }
 }
